2cf7fb4a212993270f6130709f1c7c6976b8c75b
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-10-18 01:22:34 +0200

    redux

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,2 @@
 export const getContacts = state => state.contacts;
 export const getFilter = state => state.filter;
-
-//O
-// export const selectContacts = state => state.contacts;
-// export const selectFilter = state => state.filter;
